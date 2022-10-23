00ddfea7a73b02d0e57c63fd1acca5962a8c23f0
Jerry <chr17739159896@163.com>
2022-10-23 20:26:02 +0800

    Linux练习——10_23

--- lesson15/myproc.c
+++ lesson15/myproc.c
@@ -1,36 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/wait.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 int main()
 {
-      pid_t id = fork();
-      if (id == 0){
-               //child          
-                   int count = 10;
-                         while (count--){
-                                  printf("I am child...PID:%d, PPID:%d\n", getpid(), getppid());
-                                           sleep(1);
-                                                 }
-                                                       exit(0);
-                                                          }
-                                                             //father           
-                                                             int status = 0;
-                                                                   pid_t ret = waitpid(id, &status, 0);
-                                                                      if (ret >= 0){
-                                                                            //wait success                    
-                                                                                  printf("wait child success...\n");
-                                                                                        if (WIFEXITED(status)){
-                //exit normal                                 
-                                                                                                          printf("exit code:%d\n", WEXITSTATUS(status));
-                                                                                                                }
-                                                                                                                      else{
-                                                                                                                               //signal killed                              
-                                                                                                                                        printf("killed by siganl %d\n", status & 0x7F);
-                                                                                                                                              }
-                                                                                                                                                 }
-                                                                                                                                                    sleep(3);
-                                                                                                                                                       return 0;
-                                                                                                                                                       }
-               
+   int i;
+      pid_t ids[10];
+      for ( i = 0; i < 10; ++i){
+               pid_t id = fork();
+               if (id == 0){
+                           //child
+                                  printf("child process created successfully...PID:%d\n", getpid());
+                                           sleep(3);
+                                                    exit(i); //将子进程的退出码设置为该子进程PID在数组ids中的下标
+                                                         
+               }
+                     //father
+                         ids[i] = id;
+                     
+      }
+      for ( i = 0; i < 10; ++i){
+               int status = 0;
+                     pid_t ret = waitpid(ids[i], &status, 0);
+                     if (ret >= 0){
+                                 //wait child success
+                                        printf("wiat child success..PID:%d\n", ids[i]);
+                                                 if (WIFEXITED(status)){
+                                 //                            //exit normal
+                                                                         printf("exit code:%d\n", WEXITSTATUS(status));
+                                                                                  }
+                                                                                           else{
+                                                                                                       //signal killed
+                                                                                                                   printf("killed by signal %d\n", status & 0x7F);
+                                                                                                                         }
+                                                                                                                                  }
+                                                                                                                                     }
+                                                                                                                                        return 0;
+                                                                                                                                        }
+                                 
+                                 
