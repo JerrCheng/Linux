b4bb2794ddf033b3eb1f7bab936f33c546798f80
Jerry <chr17739159896@163.com>
2022-11-10 15:21:01 +0800

    Linux练习——11_10

---
+++ lesson_11_10/myproc.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+int main()
+{
+      //c
+       printf("hello printf\n");
+          fputs("hello fputs\n", stdout);
+             //system
+                write(1, "hello write\n", 12);
+                   fork();
+                      return 0;
+      
+}
